94ac7b15321b3aed30268a713d336b73af54cc62
Gousia Habib <gousiya.cstaff@iitd.ac.in>
2026-04-29 13:13:15 +0300

    Implement FeatureBlender class for feature blending

--- feature_blending.py
+++ feature_blending.py
@@ -1,1 +1,17 @@
 
+import torch
+import torch.nn as nn
+
+class FeatureBlender(nn.Module):
+    def __init__(self, num_levels):
+        super().__init__()
+        self.weights = nn.Parameter(torch.ones(num_levels))
+
+    def forward(self, features):
+        w = torch.softmax(self.weights, dim=0)
+
+        out = 0
+        for i in range(len(features)):
+            out = out + w[i] * features[i]
+
+        return out
